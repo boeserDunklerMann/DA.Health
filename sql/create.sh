256df7bad5c9ad22fa61aeadfeab949cea5a2045
boeserDunklerMann <andre.dunkel@o2mail.de>
2019-08-15 12:06:34 +0200

    we

--- sql/create.sh
+++ sql/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mysql -ponly4sus < ./0000_InitDB.sql
+mysql -p<insertRootPasswdHere> < ./0000_InitDB.sql
 
 #find ./ -maxdepth 1 -type f -name '*.sql' -execdir ./exec.sh {} \; | sort
 #find ./ -maxdepth 1 -type f -name '*.sql' -execdir cat \< {} \; | sort
